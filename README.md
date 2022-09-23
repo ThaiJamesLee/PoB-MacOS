826f4f7f7864eddf5caa56b6cb286bd76fd554ab
Duc Tai Ly <ductaily@yahoo.de>
2022-09-23 16:47:02 +0200

    add install script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install PoB on MacOS
+
+Copied from: <https://www.pathofexile.com/forum/view-thread/3009944>
+
+# Install PoB
+
+```sh
+bash <(curl -s http://mywebsite.example/myscript.txt)
+```
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+POB_GH_LINK=https://github.com/hsource/pobfrontend/releases/latest/download/PathOfBuilding.zip
+
+cd ~/Downloads
+
+if [ -d PathOfBuilding ]; then
+    rm -rf PathOfBuilding
+fi
+
+echo "GET ${POB_GH_LINK}"
+curl -L $POB_GH_LINK --output PathOfBuilding.zip
+
+echo "Extract..."
+unzip -o PathOfBuilding.zip -d pob_tmp
+
+echo "Move PathOfBuilding to Applications..."
+mv -f pob_tmp/PathOfBuilding.app /Applications
+
+echo "Clean up..."
+rm -rf pob_tmp
